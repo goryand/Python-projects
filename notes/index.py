6b2a634ac16a4371f68e2b480fbd4935dc367acd
goryand <59327476+goryand@users.noreply.github.com>
2023-05-25 02:01:12 -0400

    Update index.py

--- notes/index.py
+++ notes/index.py
@@ -26,7 +26,22 @@ for iFruit in Fruits:
 
 range(7)  # -> [0,1,2,3,4,5,6]
 
+tasteLog = {
+    "Big Mac": "very tasty",
+    "Broccoli": "tasty"
+}
 
+tasteLog["McNuggets"] = "very tasty"
+tasteLog["Coca Cola"] = "Godlike tasty"
+
+# Broccoli sucks, lets delete it
+# i am laughing to myself doing this at may 25 2023 1:56 AM
+del tasteLog["Broccoli"]
+
+# I forgot if I added Candy to the list, but dont want an error. I will utilize .get
+candyTaste = tasteLog.get("Candy")
+print(candyTaste)
+# -> returns 'None', which is nil or null
 
 
 
