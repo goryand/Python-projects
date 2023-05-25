558946c5151ff3bbc5b82f84dbba915304954ca6
goryand <59327476+goryand@users.noreply.github.com>
2023-05-25 02:39:28 -0400

    Update index.py
    yeaaa boiiiii

--- Space Calculator/index.py
+++ Space Calculator/index.py
@@ -1,1 +1,17 @@
-print("Space calculator time!")
+
+
+# Calculates the amount of people in space, and lists their names and respective spacecrafts
+
+import requests
+
+response = requests.get('http://api.open-notify.org/astros.json')
+json = response.json()
+
+print("There are a total of " + str(json['number']) + " people in space. Their names are:")
+for person in json['people']:
+    print(person['name'] + " of the spacecraft " + person['craft'])
+    
+
+
+
+
